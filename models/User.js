cbbcda435e0805eb01eab1358aa74445ba29d331
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-01 23:02:22 +0800

    Added jsonwebtoken

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
 	username: {
@@ -38,6 +39,10 @@ UserSchema.pre('save', async function (next) {
 
 UserSchema.methods.matchPasswords = async function (password) {
 	return await bcrypt.compare(password, this.password)
+}
+
+UserSchema.methods.getSignedToken = function() {
+
 }
 
 const User = mongoose.model('User', UserSchema)
