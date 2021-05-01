e5e70b69f59de6663e72e12745a44f80bed29b21
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-02 01:27:05 +0800

    Set rejectUnauthorized: false, and secure: false, of transporter function but di naka receive ng message kahit successful na

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,29 +1,47 @@
 const nodemailer = require('nodemailer')
 
-const sendEmail = (options) => {
-  const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    }
-  })
+const sendEmail = options => {
+	const transporter = nodemailer.createTransport({
+		secure: false,
+		service: process.env.EMAIL_SERVICE,
+		auth: {
+			user: process.env.EMAIL_USERNAME,
+			pass: process.env.EMAIL_PASSWORD,
+		},
+		tls: {
+			rejectUnauthorized: false,
+		},
+	})
 
-  const mailOptions = {
-    from: process.env.EMAIL_FROM,
-    to: options.to,
-    subject: options.subject,
-    html: options.text,
-  }
+	// var transporter = nodemailer.createTransport(
+	// 	smtpTransport({
+	// 		host: 'outmail.abc.co.th', // hostname
+	// 		secure: false, // use SSL
+	// 		port: 25, // port for secure SMTP
+	// 		auth: {
+	// 			user: 'username@abc.co.th',
+	// 			pass: 'passwordmail',
+	// 		},
+	// 		tls: {
+	// 			rejectUnauthorized: false,
+	// 		},
+	// 	})
+	// )
 
-  transporter.sendMail(mailOptions, function(err, info) {
-    if(err) {
-      console.log(err)
-    } else {
-      console.log(info)
-    }
-    
-  })
+	const mailOptions = {
+		from: process.env.EMAIL_FROM,
+		to: options.to,
+		subject: options.subject,
+		html: options.text,
+	}
+
+	transporter.sendMail(mailOptions, function (err, info) {
+		if (err) {
+			console.log(err)
+		} else {
+			console.log(info)
+		}
+	})
 }
 
 module.exports = sendEmail
