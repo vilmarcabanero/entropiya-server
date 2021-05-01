690e399e48d075d7350022c4797004fb3767732a
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-02 00:42:35 +0800

    Done setting up the forgot password.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const ErrorResponse = require('../utils/errorResponse')
+const sendEmail = require('../utils/sendEmail')
 
 exports.register = async (req, res, next) => {
 	const { username, email, password } = req.body
@@ -78,13 +79,25 @@ exports.forgotpassword = async (req, res, next) => {
 		`
 
 		try {
-				
-		} catch (error) {
-			
+			await sendEmail({
+				to: user.email,
+				subject: 'Password Reset Request',
+				text: message
+			})
 
+			res.status(200).json({ success: true, data: 'Email Sent'})
+		} catch (error) {
+			user.resetPasswordToken = undefined
+			user.resetPasswordExpire = undefined
+
+			await user.save()
+
+			return next(new ErrorResponse('Email could not be sent.', 500))
 		}
 
-	} catch (error) {}
+	} catch (error) {
+		next(error)
+	}
 }
 
 exports.resetpassword = (req, res, next) => {
