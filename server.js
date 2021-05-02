1875243794381a695fa1ede9b1e8f744153fb7f5
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-02 15:46:23 +0800

    Cors

--- server.js
+++ server.js
@@ -19,8 +19,19 @@ app.use('/api/private', require('./routes/private'))
 //Error Handler (Should be last piece of middleware)
 app.use(errorHandler)
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://www.entropiya.ph/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/', (req, res) => {
 	res.send('Hello World')
+})
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
 app.get('/players', (req, res) => {
